76e8fce872c40520f073c688c45e5f15524ad66d
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-07-25 13:21:10 +0530

    Added all the pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Notifications from "./pages/Notifications";
 
-function App() {
-  const [count, setCount] = useState(0)
+{/* {import.meta.env.VITE_REACT_TESTING_ENV} */ }
+
+const AppContent= () => {
+  const location = useLocation();
+
+  const showNavbarAndFooter = location.pathname !== "/signup" && location.pathname !== "/login";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {showNavbarAndFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/notifications" element={<Notifications/>}/>
+      </Routes>
+      {showNavbarAndFooter && <Footer />}
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
 }
 
-export default App
+export default App;
