8aae8a2fe8e6cbc144a38eb75356592670f30440
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-07-27 23:45:31 +0530

    Updated images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,14 +7,14 @@ const CarouselComponent = () => {
     return (
         <Carousel autoPlay interval={3000} infiniteLoop showThumbs={false}>
             <div className="h-[300px] md:h-[500px]"> {/* Tailwind class to control the height */}
-                <img src="https://i.pinimg.com/564x/10/cb/ae/10cbae7abbf7a5145fd571dbab47a79c.jpg" alt="Slide 1" className="object-cover h-full w-full" />
+                <img src="https://assets.architecturaldesigns.com/plan_assets/358796051/large/623314DJ_Render_1702999278.webp" alt="Slide 1" className="object-cover h-full w-full" />
             </div>
             <div className="h-[300px] md:h-[500px]"> {/* Tailwind class to control the height */}
-                <img src="https://i.pinimg.com/564x/b9/5f/5e/b95f5efdfc791dfc66148148b46ff0b9.jpg" alt="Slide 2" className="object-cover h-full w-full" />
+                <img src="https://assets.architecturaldesigns.com/plan_assets/348164464/large/623193DJ_Render-01.2_1699305294.webp" alt="Slide 2" className="object-cover h-full w-full" />
                 
             </div>
             <div className="h-[300px] md:h-[500px]"> {/* Tailwind class to control the height */}
-                <img src="https://i.pinimg.com/564x/92/09/ce/9209ceb32a7d64976d17e003e2dd7355.jpg" alt="Slide 3" className="object-cover h-full w-full" />
+                <img src="https://assets.architecturaldesigns.com/plan_assets/363604627/large/666278RAF_Render_001_1715780244.webp" alt="Slide 3" className="object-cover h-full w-full" />
                 
             </div>
         </Carousel>
